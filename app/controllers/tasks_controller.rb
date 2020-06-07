58584c2a3e6b2fc3aa55949e27ef1d77bd352c56
yujin.noguchi <yujin.tvxq@gmail.com>
2020-06-07 11:29:12 +0000

    validation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -52,6 +52,10 @@ class TasksController < ApplicationController
   
   
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
   
   def task_params
     params.require(:task).permit(:content)

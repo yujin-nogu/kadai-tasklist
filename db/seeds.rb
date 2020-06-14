d1e5680827ac729fc577a793238073926e62614e
yujin.noguchi <yujin.tvxq@gmail.com>
2020-06-14 19:44:04 +0000

    add bootstrap design

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 (1..100).each do |number|
-  Task.create(title: 'test title ' + number.to_s, content: 'test content ' + number.to_s)
+  Task.create(status: 'test status ' + number.to_s, content: 'test content ' + number.to_s)
 end
 

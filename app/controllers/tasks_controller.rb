99cf598f6f3b763be256cce04594b7ac4c7d9d1d
yujin.noguchi <yujin.tvxq@gmail.com>
2020-06-21 22:51:52 +0000

    post-2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,13 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+    before_action :correct_user, only: [:destroy]
   
   def index
-    @tasks = Task.all
-    @tasks = Task.all.page(params[:page]).per(3)
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
   
   def show
@@ -14,18 +18,20 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  
+  
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
+      flash[:success] = 'Taskを投稿しました。'
+      redirect_to root_url
     else
-      flash.now[:danger] = 'Task が投稿されませんでした'
-      render :new
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      flash.now[:danger] = 'Taskの投稿に失敗しました。'
+      render 'tasks/index'
     end
-    
   end
+  
 
   def edit
     # @task = Task.find(params[:id])
@@ -43,12 +49,11 @@ class TasksController < ApplicationController
     end
   end
 
+  
   def destroy
-    # @task = Task.find(params[:id])
     @task.destroy
-
-    flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    flash[:success] = 'Taskを削除しました。'
+    redirect_back(fallback_location: root_path)
   end
   
   
@@ -60,6 +65,13 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @taskt = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
     get 'signup', to: 'users#new'
     resources :users, only: [:index, :show, :new, :create]    
     
+    resources :tasks, only: [:create, :destroy]
 end

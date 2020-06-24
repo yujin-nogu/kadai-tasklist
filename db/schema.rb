7658de377bb5e96b0cc88466d546beaad64531c3
yujin.noguchi <yujin.tvxq@gmail.com>
2020-06-24 13:26:56 +0000

    db-table reset

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,9 @@ ActiveRecord::Schema.define(version: 2020_06_21_113023) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
-    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.bigint "user_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
